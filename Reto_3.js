c35313f476af4ffd2b88cf0c9ab93f2d209eab08
Jorge27112 <47423961+Jorge27112@users.noreply.github.com>
2019-02-07 09:39:41 -0600

    Create Reto_3.js

---
+++ Reto_3.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225);
+  
+  stroke(255,0,0);
+  strokeWeight(25);
+  ellipse(200,200,300,300);
+  
+  strokeWeight(25);
+  line(100,100,300,300);
+}
